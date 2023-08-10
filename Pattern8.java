b97d767c2548513c4b61452157f6d978c1443023
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-10 11:30:12 +0530

    Add files via upload

---
+++ Pattern8.java
@@ -0,0 +1,11 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            for(int j=i;j<=6;j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+
